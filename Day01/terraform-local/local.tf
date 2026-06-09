5c3bd44433373d8e20c7bb25f9b5a9b3099f5d46
harshit-webknot <harshit.sahu@webknot.in>
2026-06-09 22:00:40 +0530

    Added Day01

--- Day01/terraform-local/local.tf
+++ Day01/terraform-local/local.tf
@@ -12,3 +12,13 @@ resource "local_file" "devops" {
 	filename = "hello.txt"
 	content = "This is my test file"
 }
+
+resource "random_string" "rand-str" {
+	length = 16
+	special = true
+	override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "rand-str" {
+value = random_string.rand-str[*].result
+}
